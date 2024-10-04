03cc5b1e6870cbb0be423837d9bc1b2aef0f88ab
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-04 12:19:00 +0100

    Task 1. Let's make some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const classRoomObj1 = new ClassRoom(19);
+  const classRoomObj2 = new ClassRoom(20);
+  const classRoomObj3 = new ClassRoom(34);
+  const classArray = [classRoomObj1, classRoomObj2, classRoomObj3];
+
+  return classArray;
+}
