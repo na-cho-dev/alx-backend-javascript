a387eadda9bb6d1ce7a077664c85e3fa96f53b01
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-04 20:27:23 +0100

    Task 5. A Building

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classArray = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  const classArray = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34),];
 
   return classArray;
 }
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage'
+        );
+      }
+    }
+  }
+
+  // Amount Getter/Setter
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
