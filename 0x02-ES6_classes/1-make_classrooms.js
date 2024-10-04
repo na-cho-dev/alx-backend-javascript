4598fd576c3e3116be00c47cf509d4d4b4565d20
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-04 12:30:16 +0100

    Task 1. Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,10 +2,7 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classRoomObj1 = new ClassRoom(19);
-  const classRoomObj2 = new ClassRoom(20);
-  const classRoomObj3 = new ClassRoom(34);
-  const classArray = [classRoomObj1, classRoomObj2, classRoomObj3];
+  const classArray = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 
   return classArray;
 }
--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+}
