97f57a53617a43573a268e729ec5a510c114c294
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-04 12:58:57 +0100

    1. Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 import ClassRoom from './0-classroom';
 
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
 export default function initializeRooms() {
   const classArray = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 
--- 0x02-ES6_classes/2-hbtn_course.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-class HolbertonCourse {
-  constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
-  }
-}
