9b72a9da05304592794a8a4f4c6917423a4e93df
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-05 07:43:42 +0100

    Task 2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
-
-  return promise;
+    .finally(() => console.log('Got a response from the API'));
 }
