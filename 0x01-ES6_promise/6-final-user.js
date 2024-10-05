625849d6fa4de27756a72cdc40273f300950ab1e
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-05 08:59:53 +0100

    Task 6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,24 @@
-#!/usr/bin/node
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((resolve) =>
-    resolve.map((response) => ({
-      status: response.status,
+  ]).then((values) => [
+    {
+      status: values[0].status,
       value:
-        response.status === 'fulfilled'
-          ? response.value
-          : String(response.reason),
-    }))
-  );
+        values[0].status === 'fulfilled'
+          ? values[0].value
+          : String(values[0].reason),
+    },
+    {
+      status: values[1].status,
+      value:
+        values[1].status === 'fulfilled'
+          ? values[1].value
+          : String(values[1].reason),
+    },
+  ]);
 }
