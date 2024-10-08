42f48bf77de46b8d65337d6ce0f15ca511d8cf5a
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-08 21:59:55 +0100

    Task 0. Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+export default function getListStudents() {
+  const myObjArray = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+
+  return myObjArray;
+}
