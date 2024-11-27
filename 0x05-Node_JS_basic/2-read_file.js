dca6b5c26e9b2ccd0852dabc6e601c98214c1733
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-27 15:43:18 +0100

    Task 2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    let data = fs.readFileSync(path, 'utf-8').toString().split('\n');
+    const obj = {};
+
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+
+    data.forEach((el) => {
+      const student = el.split(',');
+
+      if (!obj[student[3]]) obj[student[3]] = [];
+      obj[student[3]].push(student[0]);
+    });
+
+    for (const cls in obj) {
+      if (cls)
+        console.log(
+          `Number of students in ${cls}: ${obj[cls].length}. List: ${obj[
+            cls
+          ].join(', ')}`
+        );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
